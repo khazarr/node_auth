54feb07b71e2adebdd7d63580cb4b737ec36d8eb
Karol Swieca <swieca.karol@gmail.com>
2018-07-14 14:29:05 +0200

    foundation

---
+++ controllers/users.js
@@ -0,0 +1,20 @@
+module.exports = {
+    signUp: async (req, res, next) => {
+        console.log(`UsersController.signUp called`)
+        // console.log(req)
+        // console.log(res)
+        // console.log(next)
+    },
+    signIn: async (req, res, next) => {
+        console.log(`UsersController.signIn called`)
+        // console.log(req)
+        // console.log(res)
+        // console.log(next)
+    },
+    secret: async (req, res, next) => {
+        console.log(`UsersController.secret called`)
+        // console.log(req)
+        // console.log(res)
+        // console.log(next)
+    }
+}
